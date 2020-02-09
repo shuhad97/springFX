3e63579af2a9f272724768b362af94d62e54c821
shuhad97 <30422131+shuhad97@users.noreply.github.com>
2020-02-09 23:31:06 +0000

    Initial

---
+++ src/main/java/com/example/restservices/Rates.java
@@ -0,0 +1,27 @@
+package com.example.restservices;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rates {
+
+    private final Map<String,Double> rates;
+
+    
+
+    public Rates (HashMap<String, Double> rates){
+
+
+        
+    }
+    
+
+
+
+
+
+
+
+
+
+}
